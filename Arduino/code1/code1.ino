4424c27692c791b4ae3924066f9af6ade0841908
Dexter <ramya.austrax239@gmail.com>
2024-10-01 12:46:22 +0530

    Microcontroller Assignments

---
+++ Arduino/code1/code1.ino
@@ -0,0 +1,21 @@
+#define LED_PIN 7
+#define BUTTON_PIN 8
+
+void setup() 
+{
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+
+}
+
+void loop() 
+{
+  if(digitalRead(BUTTON_PIN) == HIGH)
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
